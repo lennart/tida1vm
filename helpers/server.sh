5ebd89df45bba172ac13ab034ea5267b9e135475
Lennart Melzer <lennart@melzer.it>
2016-07-28 11:33:04 +0200

    [++] debug flagging

--- helpers/server.sh
+++ helpers/server.sh
@@ -4,12 +4,12 @@ echo "Daumenkino Server"
 cd Daumenkino
 CURRENT_IP=$(ip add show eth0 | sed -n 's/\s*inet \([^\/]*\).*/\1/p')
 
-if [ -z "$DEBUG" ]; then
-    echo "NORMAL STARTUP"
-cabal configure \
-    && DAUMENKINO_IP=$CURRENT_IP cabal run Daumenkino
-else
-    echo "Starting XTERM, debug-mode"
+
+if [ "$DEBUG" = "1" ]; then
     xterm
+else
+    cabal configure \
+        && DAUMENKINO_IP=$CURRENT_IP cabal run Daumenkino
 fi
+
 
